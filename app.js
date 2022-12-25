e79c0ea4b4d8ed8fbc2b76490c0e747c538ce02b
jaswanthkumar1999 <90512979+jaswanthkumar1999@users.noreply.github.com>
2022-12-25 11:17:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+// let heading = React.createElement("h1", {id:"color"}, "Hello World!");
+// let rootele = ReactDOM.createRoot(document.getElementById("heading"));
+// rootele.render(heading)
+
+let container = React.createElement("div", {id:"container"}, []);
+let rootele = ReactDOM.createRoot(document.getElementById("heading"));
+rootele.render(container)
