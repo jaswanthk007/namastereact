7fa2d1de53378adde6744f7f3459fbf82054d276
jaswanthkumar1999 <90512979+jaswanthkumar1999@users.noreply.github.com>
2022-12-25 11:22:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-// let heading = React.createElement("h1", {id:"color"}, "Hello World!");
-// let rootele = ReactDOM.createRoot(document.getElementById("heading"));
-// rootele.render(heading)
-
-let container = React.createElement("div", {id:"container"}, []);
+let heading = React.createElement("h1", {id:"color"}, "Hello World!");
 let rootele = ReactDOM.createRoot(document.getElementById("heading"));
-rootele.render(container)
+rootele.render(heading)
+
+
